5c5f1b98cbb69806c5598e6bffc8c597053b39b3
chen_anguo <cag2050@163.com>
2019-03-21 19:57:36 +0800

    init

---
+++ src/HelloWorld.go
@@ -0,0 +1,10 @@
+// 必须在源文件中非注释的第一行指明这个文件属于哪个包，如：package main。
+// package main表示一个可独立执行的程序，每个 Go 应用程序都包含一个名为 main 的包。
+package main
+
+import "fmt"
+
+func main() {
+	/* 这是我的第一个简单的程序 */
+	fmt.Println("Hello, World!")
+}
