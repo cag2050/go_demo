e5b67aa6fd330cfb837d5d46c9cb112cc21e0b60
chen_anguo <cag2050@163.com>
2019-03-26 20:32:47 +0800

    update: switch

--- src/switch_value.type.go
+++ src/switch_value.type.go
@@ -1,11 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	var a interface{} = "55"
-	//var a  = "55"
-	switch a.(type) {
+	// type 只能用在 switch 语句中
+	switch vv := a.(type) {
+	// interface{} 匹配所有类型；switch 找到第一个成功的 case，执行完就会退出。
+	case interface{}:
+		fmt.Println("interface{}")
 	case string:
-		println("string")
+		fmt.Printf("vv type = %T, vv value = %+v, a type = %T, a value = %+v\n", vv, vv, a, a)
 	}
 
 	var marks = 90
